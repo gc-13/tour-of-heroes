2e29720206c32f7d20e0b8982dffb6e62cb73ee4
Gabrielle Ceresa <gmc9st@virginia.edu>
2020-06-22 20:18:00 -0400

    Imported HttpClient in the root appModule - this is used to communicate with a remote server over HTTP. Imported the In_memory Web API module to appModule to mimic communication with an actual remote data server. Generated InMemoryData Service which now holds the mock-hero data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -21,7 +24,11 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
